fac7a9ec1aab83a7bc27aa1374f67082f9ff75db
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-27 15:31:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo } from 'react';
 import '../styles/App.css';
-
 
 const isArmstrongNumber = (number) => {
   console.log("Expensive calculation execution");
@@ -11,25 +10,24 @@ const isArmstrongNumber = (number) => {
     let remainder = temp % 10;
     sum += remainder ** numberOfDigits;
     temp = parseInt(temp / 10);
-   }
+  }
   return sum === number;
 };
+
 const App = () => {
   const [num, setNum] = useState('');
   const [count, setCount] = useState(1);
-  const isArmstrong = isArmstrongNumber(num);
- 
+  const isArmstrong = useMemo(() => isArmstrongNumber(num), [num]);
+
   return (
     <div className="App">
-      <input id='input' value={num} onChange={(e) => setNum(+e.target.value)}/>
+      <input id='input' value={num} onChange={(e) => setNum(+e.target.value)} />
       <h2 id='answer'>{num ? (isArmstrong ? `${num} is armstrong` : `${num} is not armstrong`) : `Please Enter Number `}</h2>
-      <h4 id='count'onClick={() => { setCount((count) => count + 1);}}>
+      <h4 id='count' onClick={() => { setCount((count) => count + 1); }}>
         Click me to increase count:-  {count}
       </h4>
-
     </div>
   );
 }
-
 
 export default App;
